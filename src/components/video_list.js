2684a71724e07244856ad56efd302f2048478b24
vinney <vinvivo@users.noreply.github.com>
2017-10-18 11:15:33 -0700

    Added video_list_item component and displayed results in video_list.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+    // Later, we'll want to reference the results of the below map, so we assign it to a variable 'VideoItems'.
+    const VideoItems = props.searchResults.map( (video) => {
+        return <VideoListItem searchResults = {video} />
+    });
+
     // Video list is a designed element, and since Bootstrap is imported, can use Bootstrap classNames for UI design.
     return (
         <ul className="col-md-4 list-group">
-            {props.searchResults.length}
+            {VideoItems}
         </ul>
     );
 };
---
+++ src/components/video_list_item.js
@@ -0,0 +1,9 @@
+// jshint esversion: 6
+
+import React from 'react';
+
+const VideoListItem = (props) => {
+    return <li>Video</li>
+};
+
+export default VideoListItem;
