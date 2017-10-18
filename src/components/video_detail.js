dd4f2bf2ac22b9f972c2d64562fb84aa3dfb3430
vinney <vinvivo@users.noreply.github.com>
2017-10-18 12:38:12 -0700

    Handled undefined prop error (null props).

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -3,8 +3,13 @@
 import React from 'react';
 
 const VideoDetail = ({searchResults}) => {
+    if (!searchResults) {
+        return <div>Loading...</div>;
+    };
+    
     const videoId = searchResults.id.videoId;
     const url = `https://www.youtube.com/embed/${videoId}`;
+    
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
